b8b9f9e73c22d07a585404cc7f194ce3ce4c7eed
mpalitto <mpalitto@gmail.com>
2024-01-05 14:42:42 +0100

    Update comments for run-screen.sh

--- eWeLink-Proxy/nodejs/run-screen.sh
+++ eWeLink-Proxy/nodejs/run-screen.sh
@@ -13,30 +13,41 @@ iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination 192.16
 iptables -t nat -A PREROUTING -p tcp --dport 8081 -j DNAT --to-destination 192.168.200.1:8888
 iptables -t nat -A PREROUTING -p tcp --sport 8081 -j DNAT --to-destination 192.168.200.1:8888
 
-# Start a new screen session
+# Start a new screen session where to run both the Server and the User Interface
+# However in order to get the screen layout we need to issue the following commands
+# I am going to execute the commands on a separate background process that will wait 
+# before starting to execute the commands so that I can start the screen session
+# and the command be issued once the session is up and running
 bash <<EOF > /dev/tty &
-sleep 3
+sleep 3 # wait 3 secs for the new screen session be up and running
+# Start a new screen session with title
+# this session is for running the Server
 screen -S "sONOFF-Server" -X title "SERVER" # give it a title
 # Start a new screen session with title
+# this session is for the User Interface
 screen -S "sONOFF-Server" -X screen -t "CMD"
-#screen -S "sONOFF-Server"  -t "sONOFFserver" -d -m node sONOFFserver.mjs
 
 # Split the screen horizontally
 screen -S "sONOFF-Server" -X split
 
 # select SERVER titled session for top screen
 screen -S "sONOFF-Server" -X  select SERVER
-# select SERVER titled session for bottom screen
+# select CMD titled session for bottom screen
 screen -S "sONOFF-Server" -X focus
 screen -S "sONOFF-Server" -X select CMD
+
 # Resize the new region to have 5 lines
 # screen -S "sONOFF-Server" -p "sONOFFserver" -X resize -h 5
 
-# Run a different command in the new region
-# RUN the User Interface
-screen -S "sONOFF-Server" -p 0 -X stuff 'node sONOFFserver.mjs'
-screen -S "sONOFF-Server" -p 1 -X stuff 'source UI.sh'
-
+# RUN the Server in the bot pane (-p 0)
+screen -S "sONOFF-Server" -p 0 -X stuff 'node sONOFFserver.mjs
+'
+# RUN the User Interface in the bottom pane (-p 1)
+screen -S "sONOFF-Server" -p 1 -X stuff 'source UI.sh
+'
+# Save layout to allow to detach and re-attach and keeping the layout
 screen -S "sONOFF-Server" -X layout save default
 EOF
+
+# Start a new screen session
 screen -S "sONOFF-Server"  -m 
