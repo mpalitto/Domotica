8b2170cd2ef7dfadd40d2b2c61950f6562668a51
Matteo <mpalitto@gmail.com>
2024-01-05 15:01:17 +0000

    run-screen.sh: user prompt if session already existed

--- eWeLink-Proxy/nodejs/run-screen.sh
+++ eWeLink-Proxy/nodejs/run-screen.sh
@@ -13,8 +13,30 @@ iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination 192.16
 iptables -t nat -A PREROUTING -p tcp --dport 8081 -j DNAT --to-destination 192.168.200.1:8888
 iptables -t nat -A PREROUTING -p tcp --sport 8081 -j DNAT --to-destination 192.168.200.1:8888
 
-# Start a new screen session for the server and UI
-screen -mS "sONOFF-Server" -c .screenrc
+# # remove previous screen session if exists
+# screen -S "sONOFF-Server" -X quit
+# # Start a new screen session for the server and UI
+# screen -mS "sONOFF-Server" -c .screenrc
+
+SESSION_NAME="sONOFF-Server"
+
+# Check if a screen session with the same name already exists
+if screen -list | grep -q "$SESSION_NAME"; then
+    # Prompt the user to decide whether to attach or replace the existing session
+    read -p "A session named '$SESSION_NAME' already exists. Do you want to replace it? (y/n): " choice
+    if [ "$choice" = "y" ]; then
+        # Quit the existing session
+        screen -S "$SESSION_NAME" -X quit
+        # Start a new screen session for the server and UI
+        screen -mS "$SESSION_NAME" -c .screenrc
+    else
+        # Attach to the existing session
+        screen -r "$SESSION_NAME"
+    fi
+else
+    # Start a new screen session for the server and UI as no session exists
+    screen -mS "$SESSION_NAME" -c .screenrc
+fi
 
 # Note: the "-m" option forces an new top level screen session even if it was started with in
 # screen session
